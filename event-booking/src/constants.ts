77eab3c2d7081f0c8c5fc70677b66098fabbdea5
Deepanshu-Raghuwanshi <dipanshuraghuwanshio@gmail.com>
2025-02-25 01:36:52 +0530

    added constants

---
+++ event-booking/src/constants.ts
@@ -0,0 +1,15 @@
+export const LOCAL_STORAGE_KEYS = {
+  BOOKINGS: "bookings",
+  WAITING_LIST: "waitingList",
+  AVAILABLE_SLOTS: "availableSlots",
+};
+
+export const MAX_SLOTS = 10;
+
+export const DEFAULT_VALUES = {
+  AVAILABLE_SLOTS: 10,
+};
+
+export const MESSAGES = {
+  NO_CONFIRMED_BOOKINGS: "No confirmed bookings yet.",
+};
